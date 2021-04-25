a980b4ad6e3ce98521473c59abe4e835f52d7108
VanGomes29 <van.gomes29@gmail.com>
2021-04-25 17:49:13 +1000

    Update 02-pre-processing.py

--- src/pipeline/02-pre-processing.py
+++ src/pipeline/02-pre-processing.py
@@ -13,6 +13,12 @@ from numpy import unique
 from sklearn.feature_selection import VarianceThreshold
 
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.metrics import mean_absolute_error
+
+
 # path to open the file
 path = path= 'C:/users/vanessa/Documents/GitHub/ML-Pipeline-Hotel-booking-demand/data/hotel_bookings.csv'
 
@@ -32,8 +38,6 @@ print(db.head())
 
 # Get database information
 db.info()
-
-
 
 
 ## process to cleaning data
@@ -103,6 +107,38 @@ X = db.iloc[:, 1:c-1].values # external force
 y = db.iloc[:, c-1:c].values # velocity of charged particles
 
 
-# reshape and resize - already done with df.iloc[].values
-X = np.reshape(X, (r, 1))
-y = np.reshape(y, (r, 1)) 
+# # reshape and resize - already done with df.iloc[].values
+# X = np.reshape(X, (r, 1))
+# y = np.reshape(y, (r, 1)) 
+
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+# summarize the shape of the training dataset
+print(X_train.shape, y_train.shape)
+
+# fit the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+# evaluate the model
+yhat = model.predict(X_test)
+# evaluate predictions
+mae = mean_absolute_error(y_test, yhat)
+print('MAE: %.3f' % mae)
+'''
+# identify outliers in the training dataset
+lof = LocalOutlierFactor()
+yhat = lof.fit_predict(X_train)
+# select all rows that are not outliers
+mask = yhat != -1
+X_train, y_train = X_train[mask, :], y_train[mask]
+# summarize the shape of the updated training dataset
+print(X_train.shape, y_train.shape)
+# fit the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+# evaluate the model
+yhat = model.predict(X_test)
+# evaluate predictions
+mae = mean_absolute_error(y_test, yhat)
+print('MAE: %.3f' % mae)
+'''
